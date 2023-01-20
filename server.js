50e83c3f07e4b0b53fb121e8fa41f6b7525dd0b9
Alex Robertson <alex.robertson390@gmail.com>
2023-01-19 17:29:56 -0700

    push for current

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 let db = require('./db/db.json') 
 const fs = require('fs');
+const util = require('util');
 
 // Helper method for generating unique ids
 const uuid = require('./helpers/uuid');
@@ -26,12 +27,43 @@ app.get('*', (req,res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
+//Promise version of fs.readFile
+const readFromFile = util.promisify(fs.readFile);
+
+/**
+ *  Function to write data to the JSON file given a destination and some content
+ *  @param {string} destination The file you want to write to.
+ *  @param {object} content The content you want to write to the file.
+ *  @returns {void} Nothing
+ */
+
+const writeToFile = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\n Data written to ${destination}`)
+  );
+
+/**
+ *  Function to read data from a given a file and append some content
+ *  @param {object} content The content you want to append to the file.
+ *  @param {string} file The path to the file you want to save to.
+ *  @returns {void} Nothing
+ */
+const readAndAppend = (content, file) => {
+  fs.readFile(file, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedData = JSON.parse(data);
+      parsedData.push(content);
+      writeToFile(file, parsedData);
+    }
+  });
+};
 
 
 // GET Route for retrieving the notes json
 app.get('/api/notes', (req, res) => {
-    console.info(`${req.method} request received for feedback`);
-  
+    console.info(`${req.method} request received for notes`);
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
   });
 
@@ -44,35 +76,20 @@ app.post('/api/notes', (req, res) => {
     const { title, text } = req.body;
   
     // If all the required properties are present
-    if (title && text) {
-      // Variable for the object we will save
+    if (req.body) {
+    // Variable for the object we will save
       const newNote = {
-        title: title,
-        text: text,
+        title,
+        text,
         note_id: uuid(),
       };
   
-      readAndAppend(newNote, './db/notes.json');
-  
-      const response = {
-        status: 'success',
-        body: newNote,
-      };
-  
-      res.json(response);
+      readAndAppend(newNote, './db/db.json');
+      res.json(`Note added successfully`);
     } else {
-      res.json('Error in posting new note');
+      res.error('Error in adding tip');
     }
-    db.push(newNote);
-    fs.writeFile('../db/db.json', JSON.stringify(db), (err,res) => {
-      if(err) throw err;
-    })
-    res.json(db)
-  });
-
- 
-
-  
+});
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
